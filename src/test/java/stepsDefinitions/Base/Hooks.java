7cf97bbda7cc16a782d599545ba7861c157b8034
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-05-12 19:28:49 -0300

    colocando o hooks dentro do pacote de stepDefinitions do runner

--- src/test/java/stepsDefinitions/Base/Hooks.java
+++ src/test/java/stepsDefinitions/Base/Hooks.java
@@ -1,16 +1,21 @@
+package stepsDefinitions.Base;
+
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+
+import static drivers.DriverFactory.cleanupDriver;
+import static drivers.DriverFactory.getDriver;
 
 public class Hooks {
 
     @Before
     public void setup() {
-
+        getDriver();
     }
 
     @After
     public void tearDown() {
-
+        cleanupDriver();
     }
 
 }
