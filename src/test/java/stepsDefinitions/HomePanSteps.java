48a423d012ff3e379a5b9400af51866b26e9d7b0
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-05-12 15:16:26 -0300

    steps adicionado

---
+++ src/test/java/stepsDefinitions/HomePanSteps.java
@@ -0,0 +1,20 @@
+package stepsDefinitions;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class HomePanSteps {
+    @Dado("que eu esteja na home do pan")
+    public void que_eu_esteja_na_home_do_pan() {
+
+    }
+    @Quando("eu acessar atendimento e serviços online")
+    public void eu_acessar_atendimento_e_serviços_online() {
+
+    }
+    @Então("devo visualizar a Home Pan")
+    public void devo_visualizar_a_home_pan() {
+
+    }
+}
