93a092651b816807dfb72cb708c3fe61df176cf2
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-05-12 20:16:49 -0300

    add o construtor para o picocontainer fazer a gestão da injeção de dependencia e abrindo a home do pan

--- src/test/java/stepsDefinitions/HomePanSteps.java
+++ src/test/java/stepsDefinitions/HomePanSteps.java
@@ -1,13 +1,21 @@
 package stepsDefinitions;
 
+import PageObjects.Home.HomePageObject;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 
 public class HomePanSteps {
+
+    private HomePageObject homePageObject;
+
+    public HomePanSteps(HomePageObject homePageObject) {
+        this.homePageObject = homePageObject;
+    }
+
     @Dado("que eu esteja na home do pan")
     public void que_eu_esteja_na_home_do_pan() {
-
+        homePageObject.getDriver().get("https://www.bancopan.com.br/");
     }
     @Quando("eu acessar atendimento e serviços online")
     public void eu_acessar_atendimento_e_serviços_online() {
