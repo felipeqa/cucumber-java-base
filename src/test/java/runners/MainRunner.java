0b24ddef02de41ad08edccf13701937e1fd30c19
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-05-12 15:11:16 -0300

    criando o runner que executa em paralelo

---
+++ src/test/java/runners/MainRunner.java
@@ -0,0 +1,21 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
+        monochrome = true, dryRun = false, //dryRun é apenas pra rodar a stack e ver quais steps não foram implementados
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"},
+        tags = "@pan")
+
+public class MainRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
+
+//documentação: https://cucumber.io/docs/guides/parallel-execution/?lang=java#testng
+
