dcc4cc2d294e32d4b3c9ac4c573b6d9aa85ffdd9
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-05-12 15:15:36 -0300

    fix apontamento, agora esta apontando para o lugar correto

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
+@CucumberOptions(features = {"classpath:features"}, glue = {"stepsDefinitions"},
         monochrome = true, dryRun = false, //dryRun é apenas pra rodar a stack e ver quais steps não foram implementados
         plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"},
         tags = "@pan")
