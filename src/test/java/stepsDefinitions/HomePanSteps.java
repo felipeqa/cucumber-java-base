2946f1970391e7384d5d37259b54f92f8d088fad
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-05-12 20:40:55 -0300

    implementei o quanto até a chegada da home bank e validei o title

--- src/test/java/stepsDefinitions/HomePanSteps.java
+++ src/test/java/stepsDefinitions/HomePanSteps.java
@@ -1,16 +1,20 @@
 package stepsDefinitions;
 
 import PageObjects.Home.HomePageObject;
+import PageObjects.bankHome.BankHome;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
+import org.testng.Assert;
 
 public class HomePanSteps {
 
     private HomePageObject homePageObject;
+    private BankHome bankHome;
 
-    public HomePanSteps(HomePageObject homePageObject) {
+    public HomePanSteps(HomePageObject homePageObject, BankHome bankHome) {
         this.homePageObject = homePageObject;
+        this.bankHome = bankHome;
     }
 
     @Dado("que eu esteja na home do pan")
@@ -18,11 +22,16 @@ public class HomePanSteps {
         homePageObject.getDriver().get("https://www.bancopan.com.br/");
     }
     @Quando("eu acessar atendimento e serviços online")
-    public void eu_acessar_atendimento_e_serviços_online() {
-
+    public void eu_acessar_atendimento_e_serviços_online() throws InterruptedException {
+        homePageObject.atendimento.click();
+        homePageObject.waitForWebElementAndClick(homePageObject.servicosOnline);
+        homePageObject.waitForWebElementAndClick(homePageObject.boleto);
     }
     @Então("devo visualizar a Home Pan")
     public void devo_visualizar_a_home_pan() {
-
+        bankHome.waitForWebElementAndClick(bankHome.campoCpf);
+        String title = bankHome.getDriver().getTitle();
+        System.out.println(title);
+        Assert.assertEquals("Entrar em Área do Cliente",title);
     }
 }
