0a58a957cd076ed2c09e20b6abc680d2c424c9b9
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-05-12 20:20:27 -0300

    add metodo de espera implicita na classe base

--- src/test/java/PageObjects/PageObjectBase.java
+++ src/test/java/PageObjects/PageObjectBase.java
@@ -1,8 +1,14 @@
 package PageObjects;
 
 import drivers.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class PageObjectBase {
 
@@ -12,6 +18,16 @@ public class PageObjectBase {
 
     public WebDriver getDriver() {
         return DriverFactory.getDriver();
+    }
+
+    public void waitForWebElementAndClick (By by) {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
+    }
+
+    public void waitForWebElementAndClick (WebElement element) {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
     }
 
 }
