48caf265b325c3ed4a63d773265dd782250aa2bd
Luis Felipe Rodrigues de Oliveira <felipe_rodriguesx@hotmail.com>
2024-05-12 20:39:50 -0300

    add mais webelements na PO

--- src/test/java/PageObjects/Home/HomePageObject.java
+++ src/test/java/PageObjects/Home/HomePageObject.java
@@ -14,4 +14,10 @@ public class HomePageObject extends PageObjectBase {
     //filtrando por texto
     @FindBy(xpath = "//h2[contains(text(),'Atendimento')][@data-js='category']")
     public WebElement atendimento;
+
+    @FindBy(xpath ="//h4[contains(text(),'Serviços online')][@data-js='topico']")
+    public WebElement servicosOnline;
+
+    @FindBy(css ="a[href='https://online.bancopan.com.br/']")
+    public WebElement boleto;
 }
